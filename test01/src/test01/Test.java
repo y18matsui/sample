d61541fce58067da621b98e67736f864ecb3b1ec
y18matsui <y18matsui@xcat.co.jp>
2021-10-08 16:40:21 +0900

    新規ファイルコミット

---
+++ test01/src/test01/Test.java
@@ -0,0 +1,17 @@
+/*
+ * Test Created on 2021/09/28
+ */
+package test01;
+
+public class Test
+{
+
+	// メンバ変数
+	public String name = "Samurai";
+	
+	// メソッド
+	public String testMethod() {
+		return "Engineer";
+	}
+
+}
